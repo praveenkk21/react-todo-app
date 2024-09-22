396f7a1934966354e17189b34b97e9451822491f
Praveen Kumar B <praveenkk21@gmail.com>
2024-09-22 21:36:51 +0530

    with counter button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import  {LearningComponent} from './components/learning-examples/LearningComponent'
+import  Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <b>The praveen's Application</b>
-      <LearningComponent/>
+      <Counter/>
     </div>
   );
 }
